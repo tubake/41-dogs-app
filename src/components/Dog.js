ab4a30690d89d53f73852fd765cb36480e84506a
Tuba Bezek <tuba.bezek@bil.omu.edu.tr>
2020-05-12 00:47:50 +0300

    Homework added
    Dog.js/
    DefaultProps,
    propTypes added

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import FavoriteActions from "./FavoriteActions";
+import { Link } from "react-router-dom";
+import classes from './Dog.module.css';
+import PropTypes from 'prop-types';
 
-const Dog = ({id, name, toggle, getStatus}) => {
-    return <li key={id} style={{
-        margin: "15px"
-    }}>
-                            <span style={{
-                                display: "inline-block",
-                                marginRight: "15px"
-                            }}>
-                                {name}
-                            </span>
-        <FavoriteActions toggle={toggle} id={id} getStatus={getStatus}/>
-    </li>
+
+const Dog = ({ id, name, image }) => {
+    return (
+        <div className={classes.DogItem}>
+            <div className={classes.DogImg}>
+                <img src={image} alt={name} className={classes.Image} />
+            </div>
+
+            <div className={classes.Title}>
+                <div className={classes.Link}>
+                    <Link to={`/detail/${id}`}>
+                        {name}
+                    </Link>
+                </div>
+                <div>
+                    <FavoriteActions
+                        id={id}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
+
+Dog.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string
+};
+
+Dog.defaultProps = {
+    id: "Not found",
+    name: "Not found"
+}
 
 export default Dog;
